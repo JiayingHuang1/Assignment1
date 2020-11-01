c1f26c3bc5e526f78107365a71d131132a079474
JiayingHuang1 <44974466+JiayingHuang1@users.noreply.github.com>
2020-11-01 17:02:28 +0800

    Add files via upload

---
+++ app/Views/home.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+	<head>
+		<title>DGPT4711 assignment1</title>
+	</head>
+	<body>
+            <h1>The Godfather movie charactor</h1>
+		<p><a href="/godfather">Show the charactor</a>.</p>
+	</body>
+</html>
