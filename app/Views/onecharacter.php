5ab3c616679a04a8671338baa20323e9d4edddaf
JiayingHuang1 <644980597@qq.com>
2020-11-20 23:49:54 +0800

    Initial commit

--- app/Views/onecharacter.php
+++ app/Views/onecharacter.php
@@ -4,14 +4,14 @@
 		<title>DGPT4711 assignment1</title>
 	</head>
 	<body>
-		<h1>One Godfather character</h1>
+         <h1>One Godfather character</h1>
          <p><strong>Name:</strong> {name}</p>
-		<p><strong>height:</strong> {height}</p>
+         <p><strong>height:</strong> {height}</p>
          <p><strong>weight:</strong> {weight}</p>           
          <p><strong>Age:</strong> {Age}</p>
-		<p><strong>Country：</strong> {Country}</p>
+         <p><strong>Country：</strong> {Country}</p>
          <p><strong>Description：</strong> {description}</p>
          <p>Pic:<img src="/image/{image}"/></p>
-		<p><a href="/Godfather">characterlist</a></p>
+         <p><a href="/Godfather">characterlist</a></p>
 	</body>
 </html>
