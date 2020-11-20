347db0991584077ecbecfaace977363216a56274
JiayingHuang1 <644980597@qq.com>
2020-11-20 23:47:11 +0800

    Initial commit

--- app/Models/character.php
+++ app/Models/character.php
@@ -1,12 +1,13 @@
 <?php
 namespace App\Models;
+use App\Models\Simple\JSONModel;
 
 /*
  * Mock travel destination data.
  * Note that we don't have to extend CodeIgniter's model for now
  */
 
-class character {
+class character extends JSONModel{
 
     //mock data : an array of records
     /*protected $data = [
